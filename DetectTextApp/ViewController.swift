a1d977f2ce9fab97fcaf260890472045394c22fd
Volkan Sönmez <volkan.sonmez@etts.com.tr>
2019-11-06 09:09:25 +0300

    Initial Commit

---
+++ DetectTextApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectTextApp
+//
+//  Created by Volkan Sonmez on 6.11.2019.
+//  Copyright © 2019 Porte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
